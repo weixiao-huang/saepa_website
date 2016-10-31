58d8c96d09e8f9c41131042faa0717eca5829665
ztxcydzz <18811351753@163.com>
2016-10-31 22:46:04 +0800

    稍微修改了一下border宽度

--- app/scripts/nav.js
+++ app/scripts/nav.js
@@ -8,23 +8,23 @@
 //   $(this).removeClass('nav_hover');
 // });
 
-var img = document.getElementById("index_img");
-var nav_img = document.getElementById("nav_right_img");
+var img = document.getElementById('index_img');
+var nav_img = document.getElementById('nav_right_img');
 var nav_height = $('nav').outerHeight();
-nav_img.children[0].style.height = nav_height + "px";
+nav_img.children[0].style.height = nav_height + 'px';
 nav_height += 10;
 
 // Initial state
 for (var i = 0; i < img.childElementCount; ++i) {
-  img.children[i].style.width = document.body.offsetWidth + "px";
-  img.children[i].style.marginTop = "-" + nav_height + "px";
+  img.children[i].style.width = document.body.offsetWidth + 'px';
+  img.children[i].style.marginTop = '-' + nav_height + 'px';
 }
 
 // When window is resize
-window.addEventListener("resize", function(ev) {
+window.addEventListener('resize', function(ev) {
   for (var i = 0; i < img.childElementCount; ++i) {
-    img.children[i].style.width = document.body.offsetWidth + "px";
-    img.children[i].style.marginTop = "-" + nav_height + "px";
+    img.children[i].style.width = document.body.offsetWidth + 'px';
+    img.children[i].style.marginTop = '-' + nav_height + 'px';
   }
 });
 
