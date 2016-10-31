fb322dd6ecaeba42df923ca500bbdf3e2853b224
ztxcydzz <18811351753@163.com>
2016-10-31 23:40:16 +0800

    index图片的多余部分去掉

--- app/scripts/nav.js
+++ app/scripts/nav.js
@@ -19,6 +19,7 @@ for (var i = 0; i < img.childElementCount; ++i) {
   img.children[i].style.width = document.body.offsetWidth + 'px';
   img.children[i].style.marginTop = '-' + nav_height + 'px';
 }
+img.style.maxHeight = innerHeight - nav_height + 'px';
 
 // When window is resize
 window.addEventListener('resize', function(ev) {
