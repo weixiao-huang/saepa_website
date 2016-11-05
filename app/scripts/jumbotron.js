5ecea114520e258e7d325744ea148fed10431fe0
ztxcydzz <18811351753@163.com>
2016-11-06 01:32:13 +0800

    修改array代码风格

--- app/scripts/jumbotron.js
+++ app/scripts/jumbotron.js
@@ -1,22 +1,22 @@
 
 var img = document.getElementById('index_img');
-var nav_height = $('nav').outerHeight();
+var $nav_height = $('nav').outerHeight();
 var $arrow = $('#arrow');
 
 
 // Initial state
 for (var i = 0; i < img.childElementCount; ++i) {
   img.children[i].style.width = document.body.offsetWidth + 'px';
-  img.children[i].style.marginTop = '-' + nav_height + 'px';
+  img.children[i].style.marginTop = '-' + $nav_height + 'px';
 }
-img.style.maxHeight = innerHeight - nav_height + 'px';
+img.style.maxHeight = innerHeight - $nav_height + 'px';
 
 
 // When window is resize
 window.addEventListener('resize', function(ev) {
   for (var i = 0; i < img.childElementCount; ++i) {
     img.children[i].style.width = document.body.offsetWidth + 'px';
-    img.children[i].style.marginTop = '-' + nav_height + 'px';
+    img.children[i].style.marginTop = '-' + $nav_height + 'px';
   }
 });
 
