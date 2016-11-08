fb65dbad2a479d388934c180b5c34d61775763c7
何钦尧 <hqythu@gmail.com>
2016-11-08 21:25:43 +0800

    move slider initialization to index.js: resolve problem of invalidate js after build

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -8,6 +8,9 @@
 //   $(this).removeClass('nav_hover');
 // });
 
-var nav_img = document.getElementById('nav_right_img');
-var nav_height = $('nav').outerHeight();
-nav_img.children[0].style.height = nav_height + 'px';
+$(function () {
+  var nav_img = document.getElementById('nav_right_img');
+  var nav_height = $('nav').outerHeight();
+  nav_img.children[0].style.height = nav_height + 'px';
+});
+
