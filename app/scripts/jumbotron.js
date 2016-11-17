eb48a11b2872c896ba0303a53f82a291ca528fd0
乔逸凡 <Yifan@LegaDyan.local>
2016-11-17 15:43:31 +0800

    fix arrow bug and add arrow hide function

--- app/scripts/jumbotron.js
+++ app/scripts/jumbotron.js
@@ -10,7 +10,10 @@
     }
 
     put(windowWidth = document.body.offsetWidth, windowHeight = innerHeight) {
-      const header_height = $('nav').height() + $slider.children().height();
+
+      let picHeight = windowWidth / 16 * 9;
+      const header_height = $('nav').height() + picHeight;
+
       if (windowHeight > header_height) windowHeight = header_height;
 
       this.comp.css({
@@ -25,18 +28,32 @@
     $slider.children().css('width', document.body.offsetWidth + 'px');
     $slider.css('max-height', innerHeight - $('nav').height() + 'px');
 
-    new JumbComp($('#arrow')).put();
+    let picHeight = document.body.offsetWidth / 16 * 9;
+
+    if (innerHeight > $('nav').height() + picHeight) {
+
+      $('#arrow, #arrowbg').hide();
+      return;
+    }
+    else {
+      $('#arrow, #arrowbg').show();
+    }
+
+    new JumbComp($('#arrow'), 18.52, 25.37).put();
     new JumbComp($('#arrowbg'), 163, 48).put();
+
   }
 
   // Jquery Codes
   $(function () {
+
     setJumbotron();
     // When window is resize
     $(window).resize(() => setJumbotron());
 
     /* scroll down to container*/
-    $('#arrow, #arrowbg').click(() => $('html,body').animate(
-      { scrollTop:$('.container').offset().top }, 800));
+    $('#arrow, #arrowbg').click(() =>
+    $('html,body').animate({ scrollTop:$('.container').offset().top }, 800));
   });
+
 })();
