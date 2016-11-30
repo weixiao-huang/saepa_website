14c783843bbf2a0682bdb9dfed191339f5798d92
ztxcydzz <18811351753@163.com>
2016-12-01 07:26:12 +0800

    稍微修改一下主页的intro

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,20 +1,20 @@
 $(function () {
   $('#slider').nivoSlider();
-
-  let id = '#rotate'
-
-  $(window).on('scroll', function () {
-    for (let i = 0; i < 4; i++) {
-      if ($(document).scrollTop() + innerHeight * 0.8 > $(id + i).offset().top) {
-        $(id + i).removeClass('flipped');
-        $(id + i).addClass('flipping');
-      }
-      else {
-        $(id + i).addClass('flipped');
-        $(id + i).removeClass('flipping');
-      }
-    }
-  });
+  //
+  // let id = '#rotate'
+  //
+  // $(window).on('scroll', function () {
+  //   for (let i = 0; i < 4; i++) {
+  //     if ($(document).scrollTop() + innerHeight * 0.8 > $(id + i).offset().top) {
+  //       $(id + i).removeClass('flipped');
+  //       $(id + i).addClass('flipping');
+  //     }
+  //     else {
+  //       $(id + i).addClass('flipped');
+  //       $(id + i).removeClass('flipping');
+  //     }
+  //   }
+  // });
 });
 
 
