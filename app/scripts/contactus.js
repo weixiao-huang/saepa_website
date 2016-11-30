28b776ac4b9d8ded1d2ee53029cfcd3c12f0f11d
ztxcydzz <18811351753@163.com>
2016-12-01 05:49:48 +0800

    contact us is basically ok

--- app/scripts/contactus.js
+++ app/scripts/contactus.js
@@ -3,5 +3,19 @@
  */
 
 $(function () {
-  
+  $(window).scroll(function(){
+    $('.timeline p').each(function(){
+      const scrollTop = $(window).scrollTop(),
+        elementOffset = $(this).offset().top,
+        distance = (elementOffset - scrollTop),
+        windowHeight = $(window).height(),
+        breakPoint = windowHeight * 0.9;
+
+      if(distance > breakPoint) {
+        $(this).addClass('more_padding');
+      }  if(distance < breakPoint) {
+        $(this).removeClass('more_padding');
+      }
+    });
+  });
 });
