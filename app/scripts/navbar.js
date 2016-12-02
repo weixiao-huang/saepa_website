0b04b7b81a0571a59006e052a728085204b26584
ztxcydzz <18811351753@163.com>
2016-12-03 01:07:46 +0800

    responsive navbar

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -12,6 +12,14 @@ $(function () {
   function putNav() {
     $('#nav_right_img').children().css(
       'height', $('nav').height() - 2 + 'px');
+    let toggleHeight = 0;
+    let ulHeight = $('ul').height();
+    if (ulHeight > 53.33)
+      toggleHeight = 53.33;
+    else
+      toggleHeight =ulHeight;
+    $('.logo').children().css(
+      'max-height', toggleHeight + 'px');
   }
 
   putNav();
